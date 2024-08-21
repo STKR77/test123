5c304dcbee213e454ed919529037e0603490a023
STKR77 <shikharmundey00@gmail.com>
2024-08-21 13:01:46 +0530

    jump  almost finished

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,24 +6,53 @@ public class movement : MonoBehaviour
 { 
     Vector2 movementinput;
     public float movementspeed = 0.1f;
+    public float jumpspeed = 1f;
+    public float gravity = 9.8f;
+    public float Verticalspeed = 0;
     public void IAMovement(InputAction.CallbackContext context)
     {
         movementinput = context.ReadValue<Vector2>();
+        
+    }
 
+    public void IAJump (InputAction.CallbackContext context) 
+    {
+     if(context.started == true)
+      {
+        Debug.Log("started");
+      }
+     if (context.performed == true)
+      {
+        Debug.Log("performed");
+      }
+     if (context.canceled == true)
+      {
+         Debug.Log("cancelled");
+      }
     }
 // Start is called before the first frame update
-void Start()
+    void Start()
     {
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Translate(movementinput.x * movementspeed * Time.deltaTime, 0, movementinput.y * movementspeed * Time.deltaTime);
-       
+    { 
+        if(GroundCheck() == true)
+        {
+            Verticalspeed = 0;
+        }
+        else
+        {
+            Verticalspeed = (Verticalspeed - gravity) * Time.deltaTime;
+        }
+
+        transform.Translate(movementinput.x * movementspeed * Time.deltaTime, Verticalspeed , movementinput.y * movementspeed * Time.deltaTime);
     }
 
-
-
+    public bool GroundCheck()
+    {
+        return Physics.Raycast(transform.position, transform.up * -1, 1);
+    }
 }
