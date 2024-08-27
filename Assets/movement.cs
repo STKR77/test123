392f32dfe2c7f9840e46e4d79e10c3fc117d5cca
STKR77 <shikharmundey00@gmail.com>
2024-08-27 11:05:20 +0530

    movement and interaction

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -6,7 +6,8 @@ public class movement : MonoBehaviour
 { 
     Vector2 movementinput;
     public float movementspeed = 0.1f;
-    public float jumpspeed = 1f;
+    public Cinemachine.CinemachineVirtualCamera playerCamera;
+    public float jumpspeed = 5;
     public float gravity = 9.8f;
     public float Verticalspeed = 0;
     public void IAMovement(InputAction.CallbackContext context)
@@ -14,21 +15,19 @@ public class movement : MonoBehaviour
         movementinput = context.ReadValue<Vector2>();
         
     }
-
-    public void IAJump (InputAction.CallbackContext context) 
+    public void IAJump(InputAction.CallbackContext context)
     {
-     if(context.started == true)
-      {
-        Debug.Log("started");
-      }
-     if (context.performed == true)
-      {
-        Debug.Log("performed");
-      }
-     if (context.canceled == true)
-      {
-         Debug.Log("cancelled");
-      }
+        if(context.started == true && GroundCheck() == true)
+        {
+            Verticalspeed = jumpspeed;
+        }
+    }
+    public void IAInteract (InputAction.CallbackContext context)
+    {
+        if(context.started == true)
+        {
+            InteractionRayCast();
+        }
     }
 // Start is called before the first frame update
     void Start()
@@ -39,20 +38,32 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     { 
-        if(GroundCheck() == true)
+        if(GroundCheck() == true && Verticalspeed <= 0)
         {
             Verticalspeed = 0;
         }
         else
         {
-            Verticalspeed = (Verticalspeed - gravity) * Time.deltaTime;
+            Verticalspeed = Verticalspeed - gravity * Time.deltaTime;
         }
 
-        transform.Translate(movementinput.x * movementspeed * Time.deltaTime, Verticalspeed , movementinput.y * movementspeed * Time.deltaTime);
+        transform.Translate(movementinput.x * movementspeed * Time.deltaTime, Verticalspeed * Time.deltaTime , movementinput.y * movementspeed * Time.deltaTime);
     }
 
     public bool GroundCheck()
     {
-        return Physics.Raycast(transform.position, transform.up * -1, 1);
+        return Physics.Raycast(transform.position, transform.up * -1, 1.1f);
+    }
+    public void InteractionRayCast()
+    {
+        Vector3 myPosition = transform.position;
+        Vector3 CameraDirection = playerCamera.transform.forward;
+
+        Ray InteractionRay = new Ray(transform.position, CameraDirection)
+
+
+       RaycastaHit targetinfo;
+            
+            Physics.Raycast(InteractionRay, out targetinfo, 5f );
     }
 }
